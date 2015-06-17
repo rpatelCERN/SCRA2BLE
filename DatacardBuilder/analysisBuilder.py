6f050f01017fe9dc09e6429a3a8a7ffd90dd49b5
rpatelCERN <rpatel@cern.ch>
2015-06-18 01:30:04 +0200

    Update to the right CS numbers

--- DatacardBuilder/analysisBuilder.py
+++ DatacardBuilder/analysisBuilder.py
@@ -52,9 +52,9 @@ def getSignif(fn):
 #########################################################################################################
 if __name__ == '__main__':
 	
-	signals = ['SMSqqqq1000','SMSqqqq1400','SMStttt1200','SMStttt1500','SMSbbbb1000','SMSbbbb1500']
+#	signals = ['SMSqqqq1000','SMSqqqq1400','SMStttt1200','SMStttt1500','SMSbbbb1000','SMSbbbb1500']
 	# signals = ['SMSqqqq1000','SMSbbbb1000']
-	# signals = ['SMSbbbb1500','SMSbbbb1000']
+	signals = ['SMSbbbb1500']
 	
 	#mus = [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0];
 	mus = [1.0];
