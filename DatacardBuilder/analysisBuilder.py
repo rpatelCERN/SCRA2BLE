718509528e22dba7f5a6624b27dee2ed912019e5
rpatelCERN <rpatel@cern.ch>
2015-10-21 17:58:32 +0200

    Fix for the HadTau+LL correlation Low Stats

--- DatacardBuilder/analysisBuilder.py
+++ DatacardBuilder/analysisBuilder.py
@@ -52,18 +52,18 @@ def getSignif(fn):
 #########################################################################################################
 if __name__ == '__main__':
 	
-	#signals = ['SMSqqqq1400','SMSqqqq1000','SMSbbbb1500','SMSbbbb1000','SMStttt1500','SMStttt1200']
+	signals = ['SMSqqqq1400','SMSqqqq1000','SMSbbbb1500','SMSbbbb1000','SMStttt1500','SMStttt1200']
 	#signals = ['SMSqqqq1400']
 	#signals = ['SMSbbbb1500','SMSbbbb1000','SMStttt1500','SMStttt1200']	
-	signals = ['SMSbbbb1000']
+	#signals = ['SMSbbbb1000']
 	#mus = [0.0,0.5,1.0 ,1.5, 2.0, 2.5, 3.0,3.5, 4.0,4.5, 5.0];
 	#mus = [10000.0];
-	mus=[0.0]
+	mus=[1.0]
 	#lumis = [3.0,10.0];
 	lumis = [225];
 
 	#variations = ['allBkgs','qcdOnly','zvvOnly','llpOnly','tauOnly','allNoqcd','allNozvv','allNollp','allNotau','onlyLep','allNolep']
-	variations = ['allNotau','llpOnly', 'tauOnly']
+	#variations = ['allNotau','llpOnly', 'tauOnly']
 	# variations = ['allBkgs','allNoqcd','allNozvv','allNollp','allNotau']
 	#variations=['allNoqcd','allNozvv','allNollp','allNotau','onlyLep','allNolep']
 	#variations = ['qcdOnly' ]
@@ -71,7 +71,7 @@ if __name__ == '__main__':
 	#variations=['allNozvv']
 	# variations=['onlyLep']
 	#variations=['tauOnly']
-	#variations=['allBkgs']
+	variations=['allBkgs']
 	identifiers = [];
 	limits = [];
 	fittedMus = [];
