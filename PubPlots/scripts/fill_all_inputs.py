33e9181b3961491d2a76d89e9b6c714f47b08a40
Jack Bradmiller-Feld <jbradmil@cern.ch>
2016-12-05 11:06:55 -0800

    Added QCD deltaPhi to routine

--- PubPlots/scripts/fill_all_inputs.py
+++ PubPlots/scripts/fill_all_inputs.py
@@ -6,7 +6,7 @@ from fill_qcdrs_hists import *
 from fill_data_hists import *
 from fill_signal_hists import *
 
-def fill_all_inputs(lumi):
+def fill_all_inputs(lumi=18077.494):
 #    fill_data_hists('inputs/data_hists/RA2bin_signal.root', 'RA2bin_data', 'data_hists.root', 174)
     fill_data_hists('inputs/data_hists/Data_combined_ICHEP_RUNG.root', 'RA2bin_data', 'data_hists.root', 174)
     fill_znn_hists('inputs/bg_hists/ZinvHistos.root', 'znn_hists.root', 174, lumi/36344.959) # CR lumi from single-electron V11
@@ -14,7 +14,7 @@ def fill_all_inputs(lumi):
     fill_lostlep_hists('inputs/bg_hists/LLPrediction_notCombined.root', 'lostlep_hists.root', 174, lumi/36348.547) # CR lumi from MET V11
     fill_hadtau_hists('inputs/bg_hists/ARElog94_36.35fb_HadTauEstimation_data_formatted_V11.root', 'hadtau_hists.root', 174, lumi/36340.749) # CR lumi from single-muon V11
     fill_signal_hists('signal_hists.root', 174)
-    #fill_qcd_hists('inputs/bg_hists/qcd-bg-combine-input-24.5ifb-nov4-dashes.txt', 'qcd_hists.root', 174)
+    fill_qcd_hists('inputs/bg_hists/qcd-bg-combine-input-36.3ifb-dec01-dashes.txt', 'qcd_hists.root', 174, lumi/36348.547) # CR lumi from MET V11
 
 if __name__ == "__main__":
     import sys
