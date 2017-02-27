f62fee637e8d75cf92f678ba6ed59604ba5745b3
Jack Bradmiller-Feld <jbradmil@cern.ch>
2017-02-26 16:24:41 -0800

    scripts/fill_lostlep_hists.py

--- PubPlots/scripts/fill_all_inputs.py
+++ PubPlots/scripts/fill_all_inputs.py
@@ -10,11 +10,11 @@ from fill_signal_hists import *
 def fill_all_inputs(lumi=35862.345):
    fill_data_hists('inputs/data_hists/RA2bin_signal.root', 'RA2bin_data', 'data_hists.root', 174)
    fill_znn_hists('inputs/bg_hists/ZinvHistos.root', 'znn_hists.root', 174) # CR lumi from single-electron V11: 36344.959
-   fill_qcdrs_hists('inputs/bg_hists/master-r+s-36.3.root', 'qcdrs_hists.root', 174) # CR lumi from JetHT V11: 36346.489
-   fill_lostlep_hists('inputs/bg_hists/LLPrediction_HT5.root', 'lostlep_hists.root', 174) # CR lumi from MET V11: 35862.345
+   fill_qcdrs_hists('inputs/bg_hists/master-r+s-35.9.root', 'qcdrs_hists.root', 174) # CR lumi from JetHT V11: 36346.489
+   fill_lostlep_hists('inputs/bg_hists/LLPrediction.root', 'lostlep_hists.root', 174) # CR lumi from MET V11: 35862.345
    fill_hadtau_hists('inputs/bg_hists/ARElog116_35.9ifb_HadTauEstimation_data_formatted_V12.root', 'hadtau_hists.root', 174) # CR lumi from single-muon V11: 36340.749
    fill_signal_hists('signal_hists.root', 174)
-   fill_qcd_hists('inputs/bg_hists/qcd-bg-combine-input-36.3ifb-dec16-dashes.txt', 'qcd_hists.root', 174) # CR lumi from MET V11: 35862.345
+   #fill_qcd_hists('inputs/bg_hists/qcd-bg-combine-input-36.3ifb-dec16-dashes.txt', 'qcd_hists.root', 174) # CR lumi from MET V11: 35862.345
 
 if __name__ == "__main__":
     import sys
