05a00a4c0eaee603a0c4efafd56a245a9769257f
Jack Bradmiller-Feld <jbradmil@cern.ch>
2017-04-27 12:26:50 -0700

    T1tbtb models in table

--- PubPlots/cscripts/PrintSignalTables.C
+++ PubPlots/cscripts/PrintSignalTables.C
@@ -46,10 +46,15 @@ void PrintSignalTables() {
   hqqqq_nc->Scale(lumi);
   TH1D* hqqqq_c = (TH1D*) fin->Get("ASR/RA2bin_T1qqqq_1000_800_fast_nominal");
   hqqqq_c->Scale(lumi);
+
   TH1D* hqqqqVV_nc = (TH1D*) fin->Get("ASR/RA2bin_T5qqqqVV_1400_100_fast_nominal");
   hqqqqVV_nc->Scale(lumi);
   TH1D* hqqqqVV_c = (TH1D*) fin->Get("ASR/RA2bin_T5qqqqVV_1000_800_fast_nominal");
   hqqqqVV_c->Scale(lumi);
+  TH1D* htbtb_nc = (TH1D*) fin->Get("ASR/RA2bin_T1tbtb_1500_100_fast_nominal");
+  htbtb_nc->Scale(lumi);
+  TH1D* htbtb_c = (TH1D*) fin->Get("ASR/RA2bin_T1tbtb_1100_700_fast_nominal");
+  htbtb_c->Scale(lumi);
 
   TH1D* htt_nc = (TH1D*) fin->Get("ASR/RA2bin_T2tt_700_50_fast_nominal");
   htt_nc->Scale(lumi);
@@ -65,24 +70,32 @@ void PrintSignalTables() {
   hqq_c->Scale(0.8*lumi);
   
   for (unsigned int bin(0); bin<12; bin++) {
-    printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$  \\\\ \\hline\n", 
+    printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$  \\\\ \\hline\n", 
 	   hbbbb_nc->GetBinContent(bin+1), hbbbb_nc->GetBinError(bin+1),
 	   htttt_nc->GetBinContent(bin+1), htttt_nc->GetBinError(bin+1),
-	   hqqqq_nc->GetBinContent(bin+1), hqqqq_nc->GetBinError(bin+1),
-	   hqqqqVV_nc->GetBinContent(bin+1), hqqqqVV_nc->GetBinError(bin+1)
+	   hqqqq_nc->GetBinContent(bin+1), hqqqq_nc->GetBinError(bin+1)
 	   );
   }
 
   cout << endl << "***********" << endl << endl;
   for (unsigned int bin(0); bin<12; bin++) {
-    printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ \\\\ \\hline\n", 
+    printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$  \\\\ \\hline\n", 
 	   hbbbb_c->GetBinContent(bin+1), hbbbb_c->GetBinError(bin+1),
 	   htttt_c->GetBinContent(bin+1), htttt_c->GetBinError(bin+1),
-	   hqqqq_c->GetBinContent(bin+1), hqqqq_c->GetBinError(bin+1),
-	   hqqqqVV_c->GetBinContent(bin+1), hqqqqVV_c->GetBinError(bin+1)
+	   hqqqq_c->GetBinContent(bin+1), hqqqq_c->GetBinError(bin+1)
 	   );
   }
 
+  cout << endl << "***********" << endl << endl;
+  for (unsigned int bin(0); bin<12; bin++) {
+    printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$  \\\\ \\hline\n", 
+	   htbtb_nc->GetBinContent(bin+1), htbtb_nc->GetBinError(bin+1),
+	   hqqqqVV_nc->GetBinContent(bin+1), hqqqqVV_nc->GetBinError(bin+1),
+	   htbtb_c->GetBinContent(bin+1), htbtb_c->GetBinError(bin+1),
+	   hqqqqVV_c->GetBinContent(bin+1), hqqqqVV_c->GetBinError(bin+1)
+	   );
+  }
+  
   cout << endl << "***********" << endl << endl;
   for (unsigned int bin(0); bin<12; bin++) {
     printf("& $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ & $%3.2f \\pm %3.2f$ \\\\ \\hline\n", 
