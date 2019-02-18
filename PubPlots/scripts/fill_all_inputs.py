a0784b22b5e317f586f08d123998592a11092ede
rpatel <rpatel@cern.ch>
2019-02-18 07:51:23 -0700

    Preparing for Unblinding

--- PubPlots/scripts/fill_all_inputs.py
+++ PubPlots/scripts/fill_all_inputs.py
@@ -15,7 +15,8 @@ def fill_all_inputs(lumi=35862.345):
    fill_qcdrs_hists('inputs/bg_hists/FullRun2/master-r+s-137.4.root', 'qcdrs_hists.root', 174,15./137.4) # CR lumi from JetHT V11: 36346.489
    #fill_signal_hists('signal_hists.root', 174)
    fill_znn_hists('inputs/bg_hists/FullRun2/ZinvHistos.root', 'znn_hists.root', 174,15./137.4) # CR lumi from single-electron V11: 36344.959
-   fill_data_hists('inputs/data_hists/FullRun2/RA2bin_signal.root', 'RA2bin_data2018', 'data_hists.root', 174)
+   #fill_data_hists('inputs/data_hists/FullRun2/RA2bin_signal.root', 'RA2bin_data2018', 'data_hists.root', 174)
+   fill_data_hists('inputs/data_hists/RA2bin_signalEventList.root', 'RA2bin_data2017_ra2bin2017list', 'data_hists.root', 174)
    #fill_postfit('inputs/bg_hists/FullRun2/fitDiagnosticstestCards-allBkgs-T1tttt_2000_100-137.4-mu0.0.root', 'postfit_hists.root',174)
    #fill_qcd_hists('inputs/bg_hists/qcd-bg-combine-input-36.3ifb-dec16-dashes.txt', 'qcd_hists.root', 174) # CR lumi from MET V11: 35862.345
    #fill_qcdrs_hists('inputs/bg_hists/master-r+s-35.9.root', 'qcdrs_hists.root', 174,10./41.5) # CR lumi from JetHT V11: 36346.489
